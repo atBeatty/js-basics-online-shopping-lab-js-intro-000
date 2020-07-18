801b8019b091587cf5e10bef651173454b2ebc66
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 18:23:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ function viewCart() {
       return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}.`
 
     } else if (cart.length === 2) {
-      return `In your cart, you have, ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[i+1].itemName} at $${cart[i+1].itemPrice}.`
+      return `In your cart, you have ${cart[i].itemName} at $${cart[i].itemPrice}, and ${cart[i+1].itemName} at $${cart[i+1].itemPrice}.`
     } else if (cart.length >= 3) {
       cartList += `${cart[i].itemName} at $${cart[i].itemPrice}.`
     }
