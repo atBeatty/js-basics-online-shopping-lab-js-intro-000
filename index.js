e457a4e21862de0c5fc2a0587a36d8cc5c2f0a05
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 21:28:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -89,15 +89,15 @@ function removeFromCart(item) {
 // }
 
 function placeOrder(cardNumber) {
-
+  var purchaseMsg = '';
 
   if (typeof(cardNumber) === "undefined"){
-    return "Sorry, we don't have a credit card on file for you."
+    purchaseMsg = `Sorry, we don't have a credit card on file for you.`
+    // return "Sorry, we don't have a credit card on file for you."
   } else {
     return `Your total cost is $${total}, which will be charged to the card ${cardNumber}.`
   }
 
-  cart = [];
-  
+
   // write your code here
 }
