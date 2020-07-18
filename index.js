c7305ca652a337da85137fb5c1294c935e6bb69a
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 19:09:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,8 +48,9 @@ function total() {
   // write your code her
   var totalAmount = 0;
   for (var i = 0; i < cart.length; i++) {
+
     console.log('this is item price', cart[i])
-    totalAmount += cart[i].itemPrice;
+    totalAmount += cart[i][itemPrice];
   }
 
   return totalAmount;
