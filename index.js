528f66ec9bfded1bcb37be8ba5771ce23f0f763f
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 20:20:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -58,12 +58,12 @@ function removeFromCart(item) {
   //item
   //i want to search the cart/object.itemName and see if it exists
   for (var i = 0; i < cart.length; i++) {
-    JSON.stringify(cart[i])
+    // JSON.stringify(cart[i])
     if (item === cart[i].itemName) {
       cart.splice(i, 1);
       return cart;
     } else {
-      return alert(`That item is not in your cart.`)
+      return `That item is not in your cart.`
     }
   }
 }
