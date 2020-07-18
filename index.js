7514d8bc23604eacc254400a0bb905d4c663c62c
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 21:00:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -64,8 +64,8 @@ function removeFromCart(item) {
   for (var i = 0; i < cart.length; i++) {
     // JSON.stringify(cart[i])
     if (cart[i].itemName === item) {
-      // cart.splice(i, 1);
-      return cart.splice(i, 1);
+      cart.splice(i, 1);
+      return cart;
 
     } else {
       return `That item is not in your cart.`;
