d10bfcafac6d2e7ddebb01a2255e4e9ce8669e64
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 21:23:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -90,7 +90,8 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
 
-  if (typeof(cardNumber) === undefined){
+
+  if (typeof(cardNumber) === "undefined"){
     return "Sorry, we don't have a credit card on file for you."
   } else {
     cart = [];
