29272a1328bb312258aef913cf9cd9b29f1ec874
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 20:11:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -63,7 +63,7 @@ function removeFromCart(item) {
       cart.splice(i, 1);
       return cart;
     } else {
-      alert(`That item is not in your cart.`)
+      return alert(`That item is not in your cart.`)
     }
   }
 }
