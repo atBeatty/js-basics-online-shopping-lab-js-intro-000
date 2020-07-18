b90847bd56bb282f6f89de0305b626245cd4cbc1
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 17:51:22 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,6 +24,17 @@ function addToCart(item) {
 
 function viewCart() {
   // write your code here
+  var cartList = '';
+
+  if (cart.length === 0) {
+    return "Your shopping cart is empty."
+  } else if (cart.length === 1) {
+
+  } else if (cart.length === 2) {
+
+  } else if (cart.length >= 3) {
+
+  }
 }
 
 function total() {
