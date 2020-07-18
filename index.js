33a1fc8999b821064d69570ae0a3ff953ff87696
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 20:33:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function setCart(c) {
 
 function addToCart(item) {
  // write your code here
- var price = Math.ceiling(Math.random()*100);
+ var price = Math.ceil(Math.random()*100);
 
  var itemObject = {};
 
