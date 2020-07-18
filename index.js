e3f687aa35aa9823b3e94c289e1ec6f8832b63ef
Adam Beatty <adam.t.beatt@gmail.com>
2020-07-18 18:08:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -26,20 +26,20 @@ function viewCart() {
   // write your code here
   var cartList = '';
 
-  // for (var i = 0; i < cart.length; i++) {
-  //   if (cart.length === 0) {
-  //     return "Your shopping cart is empty."
-  //   } else if (cart.length === 1) {
-  //     return `In your cart, you have, ${}`
-  //   } else if (cart.length === 2) {
-  //
-  //   } else if (cart.length >= 3) {
-  //
-  //   }
-  // }
   for (var i = 0; i < cart.length; i++) {
-    `In your cart, you have bananas at $17.`
+    if (cart.length === 0) {
+      return "Your shopping cart is empty."
+    } else if (cart.length === 1) {
+      return `In your cart, you have, ${cart[i].itemName} at $${cart[i].itemPrice}`
+      
+    } else if (cart.length === 2) {
+
+    } else if (cart.length >= 3) {
+
+    }
   }
+
+
 
 }
 
